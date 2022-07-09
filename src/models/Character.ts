433a573c47c59ba223f76c7f88c35def17073379
Dltjdn <hana400004@naver.com>
2022-07-09 18:19:43 +0900

    [ADD] CharacterSchema 추가

---
+++ src/models/Character.ts
@@ -0,0 +1,57 @@
+import mongoose from 'mongoose';
+import { CharacterInfo } from '../interfaces/character/CharacterInfo';
+
+const CharacterSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    talk: [
+      {
+        title: {
+          type: String,
+          required: true,
+        },
+        question: {
+          type: String,
+          required: true,
+        },
+        answer: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+    createPost: {
+      type: String,
+      required: true,
+    },
+    push: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    levelNames: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+  },
+  {
+    timestamps: true, // createdAt, updatedAt 자동기록
+  }
+);
+
+export default mongoose.model<CharacterInfo & mongoose.Document>(
+  'Character',
+  CharacterSchema
+);
