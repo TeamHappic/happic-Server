b04450d67c2508387057fd7fcb1f3c67d9b0ff9e
Dltjdn <hana400004@naver.com>
2022-07-10 03:50:41 +0900

    [ADD] UserSchema 추가

---
+++ src/models/User.ts
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+import { UserInfo } from '../interfaces/user/UserInfo';
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  characterName: {
+    type: String,
+    require: true,
+  },
+  growthRate: {
+    type: Number,
+    required: true,
+  },
+  level: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: 'Character',
+  },
+  character: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+    ref: 'Character',
+  },
+  film: [
+    {
+      type: mongoose.Types.ObjectId,
+      required: true,
+      ref: 'Film',
+    },
+  ],
+});
+
+export default mongoose.model<UserInfo & mongoose.Document>('User', UserSchema);
