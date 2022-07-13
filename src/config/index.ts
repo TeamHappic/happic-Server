322606aeda6bbca4b394b90fef934da0fdb64913
songkyung <yysk_915@naver.com>
2022-07-13 21:51:50 +0900

    [ADD] jwt 설정

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,7 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 // Set the NODE_ENV to 'development' by default
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const envFound = dotenv.config();
 if (envFound.error) {
@@ -20,4 +20,13 @@ export default {
    * MongoDB URI
    */
   mongoURI: process.env.MONGODB_URI as string,
+  /**
+   * jwt Secret
+   */
+  jwtSecret: process.env.JWT_SECRET as string,
+
+  /**
+   * jwt Algorithm
+   */
+  jwtAlgo: process.env.JWT_ALGO as string,
 };
---
+++ src/interfaces/common/JwtPayloadInfo.ts
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+// JWT token 객체 안에 담아서 보내 줄 정보
+export interface JwtPayloadInfo {
+    user: {
+        id: mongoose.Schema.Types.ObjectId
+    }
+}
