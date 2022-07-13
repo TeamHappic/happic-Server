1c507d874f2adfaf5ead35d960cd5a21993a8c76
songkyung <yysk_915@naver.com>
2022-07-14 03:58:16 +0900

    [FEAT] isPosted 인자 추가  #11

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,50 +3,52 @@ import statusCode from '../modules/statusCode';
 import message from '../modules/responseMessage';
 import util from '../modules/util';
 import UserService from '../services/UserService';
+import { validationResult } from 'express-validator';
+import getToken from '../modules/jwtHandler';
 
-const today = new Date()
-
-const isSameDate = (date1,today)=>{
-    return date1.getFullYear() === today.getFullYear()
-       && date1.getMonth() === today.getMonth()
-       && date1.getDate() === today.getDate();
-}
 
 /**
  *  @route GET /home
  *  @desc Read User
  *  @access Public
  */
- const findUserById = async (req: Request, res: Response) => {
+const findUserById = async (req: Request, res: Response) => {
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res
+      .status(statusCode.BAD_REQUEST)
+      .send(util.fail(statusCode.BAD_REQUEST, message.BAD_REQUEST));
+  }
 
-    try {
-        const user = await UserService.findUserById(req.body.user.id);
-        if (!user) {
-            return res
-                .status(statusCode.NOT_FOUND)
-                .send(util.fail(statusCode.NOT_FOUND, message.NOT_FOUND));
-        }
+  try {
+    //const user = await UserService.findUserById(req.body.user.id);
+    const {userId} = req.params;
+    const data = await UserService.findUserById(userId);
 
-        const data = {
-            "name":user.name,
-            "growth_rate":user.growthRate,
-            "level":user.level,
-            "isPosted":,
-        }
-        res.status(statusCode.OK).send(
-            util.success(statusCode.OK, message.READ_USER_SUCCESS, data),
-        );
-    } catch (error) {
-        console.log(error);
-        res.status(statusCode.INTERNAL_SERVER_ERROR).send(
-            util.fail(
-                statusCode.INTERNAL_SERVER_ERROR,
-                message.INTERNAL_SERVER_ERROR,
-            ),
-        );
+    if (!data) {
+      return res
+        .status(statusCode.NOT_FOUND)
+        .send(util.fail(statusCode.NOT_FOUND, message.NOT_FOUND));
     }
+
+    //const isPosted = isSameDate(user.film,today);
+
+    res
+      .status(statusCode.OK)
+      .send(util.success(statusCode.OK, message.READ_USER_SUCCESS, data));
+  } catch (error) {
+    console.log(error);
+    res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(
+        util.fail(
+          statusCode.INTERNAL_SERVER_ERROR,
+          message.INTERNAL_SERVER_ERROR
+        )
+      );
+  }
 };
 
 export default {
-    findUserById,
+  findUserById,
 };
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,23 +1,41 @@
-import { UserResponseDto } from "../interfaces/user/UserResponseDto";
-import User from "../models/User";
+import { FilmInfo } from '../interfaces/film/FilmInfo';
+import { UserResponseDto } from '../interfaces/user/UserResponseDto';
+import Film from '../models/Film';
+import User from '../models/User';
 
-const findUserById = async (
-    userId: string,
-): Promise<UserResponseDto | null> => {
-    try {
-        const user = await User.findById(userId);
-
-        if (!user) {
-            return null;
-        }
-
-        return user;
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
+const isSameDate = (date1: Date) => {
+  const today = new Date();
+  return (
+    date1.getFullYear() === today.getFullYear() &&
+    date1.getMonth() === today.getMonth() &&
+    date1.getDate() === today.getDate()
+  );
 };
 
-export default{
-    findUserById,
+const findUserById = async (
+  userId: string
+): Promise<UserResponseDto | null> => {
+  try {
+    const user = await User.findById(userId).populate('film', 'createAt');
+
+    if (!user) {
+      return null;
+    }
+    const data = {
+      characterId: user.characterId,
+      characterName: user.characterName,
+      growthRate: user.growthRate,
+      level: user.level,
+      isPosted: isSameDate(user.film[0].createAt),
+    };
+    return data;
+
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export default {
+  findUserById,
 };
