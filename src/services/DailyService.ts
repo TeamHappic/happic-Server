11b1e395a8017f846496bf9c7c6f4febfc382a50
dltjdn <hana40004@naver.com>
2022-07-17 15:49:19 +0900

    [FEAT] count 1로 바꿈

--- src/services/DailyService.ts
+++ src/services/DailyService.ts
@@ -1,6 +1,5 @@
 import { PostBaseResponseDto } from '../interfaces/common/postBaseResponseDto';
 import { FilmCreateDto } from '../interfaces/film/FilmCreateDto';
-import { FilmInfo } from '../interfaces/film/FilmInfo';
 import Film from '../models/Film';
 import Keyword from '../models/Keyword';
 
@@ -117,7 +116,7 @@ const createDaily = async (
       writer: '62cef0997f008c29128704ed',
       photo: filmCreateDto.photo,
       keyword: keywordList,
-      year: year,
+      yaer: year,
       month: month,
     });
 
@@ -134,6 +133,72 @@ const createDaily = async (
   }
 };
 
+const deleteDaily = async (filmId: string): Promise<void> => {
+  try {
+    const film = await Film.find({ _id: filmId }, { _id: 0, keyword: 1 });
+
+    const whenId = film[0].keyword[0].toString();
+    const whereId = film[0].keyword[1].toString();
+    const whoId = film[0].keyword[2].toString();
+    const whatId = film[0].keyword[3].toString();
+
+    // #when Count - 1
+    const whenKeyword = await Keyword.find(
+      { _id: whenId },
+      { _id: 0, count: 1 }
+    );
+    let whenCount: Number = whenKeyword[0].count;
+    if (whenCount === 0) {
+      await Keyword.findByIdAndDelete(whenId);
+    } else {
+      whenCount = (whenCount as number) - 1;
+      await Keyword.findByIdAndUpdate(whenId, { count: whenCount });
+    }
+
+    // #where Count - 1
+    const whereKeyword = await Keyword.find(
+      { _id: whereId },
+      { _id: 0, count: 1 }
+    );
+    let whereCount: Number = whereKeyword[0].count;
+    if (whereCount === 0) {
+      await Keyword.findByIdAndDelete(whereId);
+    } else {
+      whereCount = (whereCount as number) - 1;
+      await Keyword.findByIdAndUpdate(whereId, { count: whereCount });
+    }
+
+    // #who Count - 1
+    const whoKeyword = await Keyword.find({ _id: whoId }, { _id: 0, count: 1 });
+    let whoCount: Number = whoKeyword[0].count;
+    if (whoCount === 0) {
+      await Keyword.findByIdAndDelete(whoId);
+    } else {
+      whoCount = (whoCount as number) - 1;
+      await Keyword.findByIdAndUpdate(whoId, { count: whoCount });
+    }
+
+    // #what Count - 1
+    const whatKeyword = await Keyword.find(
+      { _id: whatId },
+      { _id: 0, count: 1 }
+    );
+    let whatCount: Number = whatKeyword[0].count;
+    if (whatCount === 0) {
+      await Keyword.findByIdAndDelete(whatId);
+    } else {
+      whatCount = (whatCount as number) - 1;
+      await Keyword.findByIdAndUpdate(whatId, { count: whatCount });
+    }
+
+    await Film.findByIdAndDelete(filmId);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
 export default {
   createDaily,
+  deleteDaily,
 };
