896f572fa170360cbef7ba3a338bf7b28e9c81fd
songkyung <yysk_915@naver.com>
2022-07-18 01:14:34 +0900

    [FIX] conflict 파일 수정

--- src/services/FileService.ts
+++ src/services/FileService.ts
@@ -24,32 +24,7 @@ const createFile = async (link: string, fileName: string): Promise<FileResponseD
     }
 }
 
-const createFiles = async (imageList: { location: string, originalname: string }[]): Promise<FileResponseDto[]> => {
-    try {
-        const data = await Promise.all(imageList.map(async image => {
-            const file = new File({
-                link: image.location,
-                fileName: image.originalname
-            });
-
-            await file.save();
-
-            return {
-                _id: file._id,
-                link: file.link
-            };
-        }));
-
-        return data;
-
-    } catch (error) {
-        console.log(error);
-        throw error;
-    }
-
-}
 
 export default {
     createFile,
-    createFiles
 }
