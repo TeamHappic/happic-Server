ce5074fd3e8fd5e296bdc46b799d60def8b4d52f
songkyung <yysk_915@naver.com>
2022-07-21 19:42:44 +0900

    [FIX] 빌드 오류 수정

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,43 @@
-import express, { Request, Response, NextFunction } from 'express';
-import config from './config';
-const app = express();
-import connectDB from './loaders/db';
+import express, { Request, Response, NextFunction } from "express";
+const app = express(); //express 이용하여 서버 띄울거니가~
+
+import connectDB from "./loaders/db";
 import routes from './routes';
 require('dotenv').config();
-var nodeschedule = require('node-schedule');
 
-connectDB();
+connectDB(); // 몽고디비에 연결한다.
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(routes); //라우터
-// error handler
+app.use(routes);   // "routes" 폴더안의 라우터를 사용할 것
 
+// error handler
 interface ErrorType {
   message: string;
   status: number;
 }
 
-app.use(function (
-  err: ErrorType,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
+app.use(function (err: ErrorType, req: Request, res: Response, next: NextFunction) {
+
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'production' ? err : {};
+  res.locals.error = req.app.get("env") === "production" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  //res.render('error');
-  console.log(err);
-  res.json({ error: res.locals.error });
+  res.render("error");
 });
 
 app
-  .listen(process.env.PORT, () => {
+  .listen(process.env.PORT, () => { 
+    // 포트 열어주기: env에 포트있으니가 여기선 따로 안해줘도댐
     console.log(`
     ################################################
           🛡️  Server listening on port 🛡️
     ################################################
   `);
   })
-  .on('error', (err) => {
+  .on("error", (err) => {
     console.error(err);
     process.exit(1);
   });
-
-export default app;
--- src/loaders/db.ts
+++ src/loaders/db.ts
@@ -1,40 +1,27 @@
-import mongoose from 'mongoose';
-import config from '../config';
-import File from '../models/File';
-import Film from '../models/Film';
-import Keyword from '../models/Keyword';
-import User from '../models/User';
-import * as admin from 'firebase-admin';
-
-var serviceAccount = require('../../firebase-admin.json');
+import mongoose from "mongoose";
+import config from "../config"; 
+import Movie from "../models/Movie";
+import Review from "../models/Review";
 
 const connectDB = async () => {
   try {
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-    });
-
+    // .env에 적어둔 connect URI로 연결시켜준다.
+    // config에 mongoURI로 설정되어있다.
     await mongoose.connect(config.mongoURI);
 
+    // mongoose option 를 선택 
+    // autoCreate: 서버실행시 collection이 자동으로 생성되게 한다.
     mongoose.set('autoCreate', true);
 
-    User.createCollection().then(function (collection) {
-      console.log('User Collection is created!');
+    console.log("Mongoose Connected ...");
+
+    Movie.createCollection().then(function (collection) {
+      console.log("Movie Collection is created!");
     });
 
-    File.createCollection().then(function (collection) {
-      console.log('File Collection is created!');
+    Review.createCollection().then(function (collection) {
+      console.log("Review Collection is created!");
     });
-
-    Film.createCollection().then(function (collection) {
-      console.log('Film Collection is created!');
-    });
-
-    Keyword.createCollection().then(function (collection) {
-      console.log('Keyword Collection is created!');
-    });
-
-    console.log('Mongoose Connected ...');
   } catch (err: any) {
     console.error(err.message);
     process.exit(1);
