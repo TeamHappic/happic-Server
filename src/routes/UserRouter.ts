daff9ec2f6951b4a817e9297d454bc5d556bb7c8
dltjdn <hana40004@naver.com>
2022-07-22 00:49:27 +0900

    [FEAT] FCM 등록 API 수정 #19

--- src/routes/UserRouter.ts
+++ src/routes/UserRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import UserController  from '../controllers/UserController';
+import UserController from '../controllers/UserController';
+import auth from '../middleware/auth';
 
 const router: Router = Router();
 
-router.post("/",UserController.registerFcm);
-router.post("/signin", UserController.signIn);
+router.post('/signin', UserController.signIn);
 router.post('/signup', UserController.signUp);
+router.post('/push', auth, UserController.registerFcm);
 
 export default router;
