6aecb08c2de157ff0d90043929c17941239f3ed8
songkyung <yysk_915@naver.com>
2022-07-22 02:05:35 +0900

    [FEAT] mypage 관련 테스트코드 완료

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import express, { Request, Response, NextFunction } from "express";
+import express, { Request, Response, NextFunction } from 'express';
 const app = express(); //express 이용하여 서버 띄울거니가~
 
-import connectDB from "./loaders/db";
+import connectDB from './loaders/db';
 import routes from './routes';
 require('dotenv').config();
 
@@ -10,7 +10,7 @@ connectDB(); // 몽고디비에 연결한다.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(routes);   // "routes" 폴더안의 라우터를 사용할 것
+app.use(routes); // "routes" 폴더안의 라우터를 사용할 것
 
 // error handler
 interface ErrorType {
@@ -18,18 +18,22 @@ interface ErrorType {
   status: number;
 }
 
-app.use(function (err: ErrorType, req: Request, res: Response, next: NextFunction) {
-
+app.use(function (
+  err: ErrorType,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "production" ? err : {};
+  res.locals.error = req.app.get('env') === 'production' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 app
-  .listen(process.env.PORT, () => { 
+  .listen(process.env.PORT, () => {
     // 포트 열어주기: env에 포트있으니가 여기선 따로 안해줘도댐
     console.log(`
     ################################################
@@ -37,7 +41,9 @@ app
     ################################################
   `);
   })
-  .on("error", (err) => {
+  .on('error', (err) => {
     console.error(err);
     process.exit(1);
   });
+
+export default app;
